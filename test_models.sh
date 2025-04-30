738ef624d73746bf944f41799ed2a156fb1862ed
mdiberna <mdiberna@fry.wi.mit.edu>
2025-04-29 23:06:44 -0400

    push before adding modularity

--- test_models.sh
+++ test_models.sh
@@ -6,7 +6,8 @@
 DATA_DIR="data"
 RESULTS_DIR="results/sample_gene_sets"
 PROCESSED_FILE="${DATA_DIR}/sample_gene_sets.csv"
-PROJECT_NAME="aconcagua_cell"
+GENE_FEATURES="${DATA_DIR}/HeLa_essentials/essentials_uniprot.csv"
+PROJECT_NAME="sample_gene_sets"
 
 # Step 0: Create results directory if it doesn't exist
 mkdir -p ${RESULTS_DIR}
@@ -23,6 +24,7 @@ python main.py \
   --input_sep "," \
   --gene_column "genes" \
   --gene_sep ";" \
+  --gene_features ${GENE_FEATURES} \
   --batch_size 1 \
   --output_file ${RESULTS_DIR}/${PROJECT_NAME}_openai
   # --screen_info path_to_screen_info.txt \
@@ -37,6 +39,7 @@ python main.py \
   --input_sep "," \
   --gene_column "genes" \
   --gene_sep ";" \
+  --gene_features ${GENE_FEATURES} \
   --batch_size 2 \
   --output_file ${RESULTS_DIR}/${PROJECT_NAME}_anthropic
   # --screen_info path_to_screen_info.txt \
